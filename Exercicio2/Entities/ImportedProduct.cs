9ac8c5bf08fb78348926bf792ace145337888237
Vinícius Mesquita de Jesus <viniciusmesquitagit@gmail.com>
2023-03-02 14:47:17 -0300

    Primeiro Commit

---
+++ Exercicio2/Entities/ImportedProduct.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+namespace Exercicio2.Entities
+{
+    class ImportedProduct : Product
+    {
+        public double CustomFee { get; set; }   
+
+        public ImportedProduct()
+        {
+
+        }
+
+        public ImportedProduct(string name, double price, double customFee) : base(name, price)
+        {
+            CustomFee = customFee;
+        }
+
+        public double TotalPrice()
+        {
+            return Price + CustomFee;
+        }
+
+        public override string PriceTag()
+        {
+            return Name + "$" + Price + " (Customs fee: $ " + CustomFee.ToString("F2", CultureInfo.InvariantCulture) + ")";
+        }
+
+
+    }
+}
---
+++ Exercicio2/Program.cs
@@ -0,0 +1,54 @@
+﻿using Exercicio2.Entities;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Exercicio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            Console.WriteLine("Enter the number of products");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Product #{i} data");
+                Console.Write("Commom, used or imported (c/u/i)? ");
+                char type = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Price: ");
+                double price = double.Parse(Console.ReadLine());
+                if(type == 'c')
+                {
+                    list.Add(new Product(name, price));
+                }
+
+                else if(type == 'u')
+                {
+                    Console.Write("Manufacture date (DD/MM/YYYY): ");
+                    DateTime manufactureDate = DateTime.Parse(Console.ReadLine());
+                    list.Add(new UsedProduct(name, price, manufactureDate));
+                }
+
+                else
+                {
+                    Console.Write("Customs fee: ");
+                    double customsFee = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    list.Add(new ImportedProduct(name, price, customsFee));
+                }
+              
+                foreach (Product prod in list)
+                {
+                    Console.WriteLine(prod.PriceTag());
+                }
+
+
+            }
+        }
+    }
+}
